e9e1fdda3fddac0984e68f5587780e8e127cb726
Dayanatique <128723447+Dayanatique@users.noreply.github.com>
2023-03-23 11:43:32 -0500

    actividad

---
+++ dayana/2/style.cs
@@ -0,0 +1,25 @@
+.argentina{
+    background-color: blue;
+    width: 800px;
+    height: 400px;
+    background-color: aliceblue;
+    border:solid 5px
+    black;
+
+}
+.azula{
+    background-color: rgb(0, 174, 255);
+    width:800px;
+    height:126px;
+
+}
+.blancoa{
+    background-color: aliceblue;
+    width:700px;
+    height:125px;
+}
+.azula{
+    background-color: rgb(0, 183, 255);
+    width:800px;
+    height: 126px;
+}
